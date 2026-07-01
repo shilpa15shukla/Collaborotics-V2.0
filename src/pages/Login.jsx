698d826014e766baa9975a2bb45417ca74c9e564
shilpa15shukla <shilpashukla512005@gmail.com>
2026-07-01 23:28:21 +0530

    Updated backend live URL in Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,10 +13,10 @@ function Login() {
     setLoading(true);
     setError("");
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
-        email,
-        password,
-      });
+      const res = await axios.post(
+  "https://collaborotics-v2-0-1.onrender.com/api/auth/login",
+  { email, password }
+);
       localStorage.setItem("token", res.data.token);
       localStorage.setItem("user", JSON.stringify(res.data.user));
       navigate("/dashboard");
